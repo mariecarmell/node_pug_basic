59c427845223bf3aee0a0681356e0b0dd3454111
Lilia Ould Hocine <louldhocine@cmaisonneuve.qc.ca>
2023-11-17 16:24:47 -0500

    Basic version

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (request, response) => {
-  response.send("Hello World!");
+  response.render("index", {
+    groupe: "23602",
+  });
 });
 
 app.listen(3000);
